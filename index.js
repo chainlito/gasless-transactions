b60e7210a251995712f2e0d8cf603d202134931d
kirikiri <kirikiri.viking@gmail.com>
2024-10-01 08:24:45 -0600

    send zero-value gasless tx

--- index.js
+++ index.js
@@ -1,3 +1,54 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
+const { ethers } = require("ethers");
+const { createSmartAccountClient, PaymasterMode } = require("@biconomy/account");
 
 dotenv.config();
+
+// Your configuration with private key and Biconomy API key
+const config = {
+  privateKey: process.env.PRIVATE_KEY,
+  bundlerUrl: process.env.BUNDLER_URL, // <-- Read about this at https://docs.biconomy.io/dashboard#bundler-url
+  rpcUrl: "https://rpc2.sepolia.org",
+  paymasterApiKey: process.env.PAYMASTER_API_KEY
+};
+
+const main = async () => {
+  // Generate EOA from private key using ethers.js
+  let provider = new ethers.providers.JsonRpcProvider(config.rpcUrl);
+  let signer = new ethers.Wallet(config.privateKey, provider);
+  
+  // Create Biconomy Smart Account instance
+  const smartWallet = await createSmartAccountClient({
+    signer,
+    bundlerUrl: config.bundlerUrl,
+    biconomyPaymasterApiKey: config.paymasterApiKey,
+  });
+
+
+  const saAddress = await smartWallet.getAccountAddress();
+  console.log("SA Address", saAddress);
+
+  const toAddress = "0xb7f79a67bD5BEE050547f9129D69B857297A9699"; // Replace with the recipient's address
+  const transactionData = "0x123"; // Replace with the actual transaction data
+  
+  // Build the transaction
+  const tx = {
+    to: toAddress,
+    data: transactionData,
+  };
+
+  // Send the transaction and get the transaction hash
+  const userOpResponse = await smartWallet.sendTransaction(tx, {
+    paymasterServiceData: { mode: PaymasterMode.SPONSORED },
+  });
+  const { transactionHash } = await userOpResponse.waitForTxHash();
+  console.log("Transaction Hash", transactionHash);
+  
+  const userOpReceipt = await userOpResponse.wait();
+  if (userOpReceipt.success == "true") {
+    console.log("UserOp receipt", userOpReceipt);
+    console.log("Transaction receipt", userOpReceipt.receipt);
+  }
+}
+
+main();
